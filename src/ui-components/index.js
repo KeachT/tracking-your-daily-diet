4b1ded336c645f9121285c544b8b62b3b14afd76
KeachT <503.b03@gmail.com>
2023-06-11 15:32:43 +0900

    delete ObjectType Note

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -5,7 +5,5 @@
  **************************************************************************/
 
 export { default as studioTheme } from "./studioTheme";
-export { default as NoteCreateForm } from "./NoteCreateForm";
-export { default as NoteUpdateForm } from "./NoteUpdateForm";
 export { default as DailyGoalCreateForm } from "./DailyGoalCreateForm";
 export { default as DailyGoalUpdateForm } from "./DailyGoalUpdateForm";
