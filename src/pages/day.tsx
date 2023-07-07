dca6fc65df3c3bca37282d39c66cc32cbd602b8e
KeachT <503.b03@gmail.com>
2023-07-07 21:44:50 +0900

    add MealForm component to Day page

--- src/pages/day.tsx
+++ src/pages/day.tsx
@@ -1,18 +1,38 @@
-// import { useAuthenticator } from '@aws-amplify/ui-react'
-import { Text } from '@mantine/core'
+import { useState } from 'react'
+import type { NextPage } from 'next'
+import { Box, Text } from '@mantine/core'
+import { DatePickerInput } from '@mantine/dates'
 import { Layout } from '../components/layout/Layout'
 import { DailyNutrition } from '../features/dailynutrition/DailyNutrition'
+import { MealForm } from '../features/mealform/MealForm'
 
-const Day = () => {
+const Day: NextPage = () => {
   // const { user } = useAuthenticator()
+  const [value, setValue] = useState<Date | null>(new Date())
 
   return (
     <Layout title="Day">
-      {/* Day, {user && <p>こんにちは！{user.username}</p>} */}
-      <Text weight={200} size="xl" className="mb-4">
-        DailyNutritions
-      </Text>
-      <DailyNutrition />
+      <Box mb={50}>
+        <Text weight={200} size="xl" mb={20}>
+          Date
+        </Text>
+        <DatePickerInput
+          valueFormat="YYYY-MM-DD"
+          placeholder="Pick date"
+          value={value}
+          onChange={setValue}
+          maw={200}
+        />
+      </Box>
+
+      <Box mb={50}>
+        <Text weight={200} size="xl" mb={20}>
+          DailyNutritions
+        </Text>
+        <DailyNutrition />
+      </Box>
+
+      <MealForm />
     </Layout>
   )
 }
