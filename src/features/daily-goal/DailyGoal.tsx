440265d7d55329780d5f35b06a6458cc9144c268
KeachT <503.b03@gmail.com>
2023-07-28 22:41:20 +0900

    add query to daily-goal

--- src/features/daily-goal/DailyGoal.tsx
+++ src/features/daily-goal/DailyGoal.tsx
@@ -1,12 +1,67 @@
-import { useState } from 'react'
-import { Box } from '@mantine/core'
+import { useEffect, useState } from 'react'
+import { Box, Button } from '@mantine/core'
+import { CreateDailyGoalInput, UpdateDailyGoalInput } from '../../API'
+import { fetchDailyGoals, addDailyGoal, updDailyGoal } from './utils'
 import { DailyGoalNumberInput } from './DailyGoalNumberInput'
 
 export function DailyGoal() {
-  const [calories, setCalories] = useState<number | ''>('')
-  const [protein, setProtein] = useState<number | ''>('')
-  const [fat, setFat] = useState<number | ''>('')
-  const [carbohydrates, setCarbohydrates] = useState<number | ''>('')
+  const [dailyGoalId, setDailyGoalId] = useState<string>('')
+  const [calories, setCalories] = useState<number>(0)
+  const [protein, setProtein] = useState<number>(0)
+  const [fat, setFat] = useState<number>(0)
+  const [carbohydrates, setCarbohydrates] = useState<number>(0)
+  const [version, setVersion] = useState<number>(0)
+
+  const createDailyGoalInput: CreateDailyGoalInput = {
+    calories: calories,
+    protein: protein,
+    carbohydrates: carbohydrates,
+    fat: fat,
+  }
+
+  const updateDailyGoalInput: UpdateDailyGoalInput = {
+    id: dailyGoalId,
+    calories: calories,
+    protein: protein,
+    carbohydrates: carbohydrates,
+    fat: fat,
+    _version: version,
+  }
+
+  const handleClick = () => {
+    if (!dailyGoalId) {
+      addDailyGoal(
+        createDailyGoalInput,
+        setDailyGoalId,
+        setCalories,
+        setProtein,
+        setFat,
+        setCarbohydrates,
+        setVersion
+      )
+      return
+    }
+    updDailyGoal(
+      updateDailyGoalInput,
+      setDailyGoalId,
+      setCalories,
+      setProtein,
+      setFat,
+      setCarbohydrates,
+      setVersion
+    )
+  }
+
+  useEffect(() => {
+    fetchDailyGoals(
+      setDailyGoalId,
+      setCalories,
+      setProtein,
+      setFat,
+      setCarbohydrates,
+      setVersion
+    )
+  }, [])
 
   return (
     <Box>
@@ -39,6 +94,10 @@ export function DailyGoal() {
         handleChange={setCarbohydrates}
         step={1}
       />
+
+      <Button mr={50} onClick={handleClick}>
+        Save
+      </Button>
     </Box>
   )
 }
