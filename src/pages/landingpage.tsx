9883619bd5a87a478d7543f74161e3579dc67693
KeachT <503.b03@gmail.com>
2023-08-14 19:36:00 +0900

    add components LandingPage

--- src/pages/landingpage.tsx
+++ src/pages/landingpage.tsx
@@ -1,15 +1,5 @@
-import { useDisclosure } from '@mantine/hooks'
-import { AuthenticatorModal } from '../components/AuthenticatorModal'
+import { LandingPage } from '../components/LandingPage'
 
-export default function LandingPage() {
-  const [opened, { open, close }] = useDisclosure(false)
-
-  return (
-    <div>
-      <p>Landing Page</p>
-      <button onClick={open}>auth</button>
-
-      {opened && <AuthenticatorModal opened={opened} close={close} />}
-    </div>
-  )
+export default function LP() {
+  return <LandingPage />
 }
