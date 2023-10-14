453c45c6c24a2af18ba3c1c9948b7cf2d889a0e5
KeachT <503.b03@gmail.com>
2023-10-14 19:56:16 +0900

    week display average weekly nutrition

--- src/pages/week.tsx
+++ src/pages/week.tsx
@@ -1,11 +1,40 @@
 import { Box, Text } from '@mantine/core'
+import { useEffect } from 'react'
 
 import { Layout } from '../components/Layout'
 import { CaloriesChart } from '../features/calories-chart'
 import { DatePickerCard } from '../features/date-picker-card'
 import { Nutritions } from '../features/nutritions'
+import { useCurrentDateStore } from '../stores/currentDate'
+import { useWeeklyMealCategoriesStore } from '../stores/weeklyMealCategories'
+import { fetchWeeklyMealCategories } from './api/fetchWeeklyMealCategories'
+import {
+  createAvgWeekNutritionValues,
+  createWeekDateString,
+} from './utils/week'
 
 export default function Week() {
+  const { currentDate } = useCurrentDateStore()
+  const { weeklyMealCategories, setWeeklyMealCategories } =
+    useWeeklyMealCategoriesStore()
+
+  const { currentDateString, prevWeekDateString } =
+    createWeekDateString(currentDate)
+  const {
+    avgWeeklyCalories,
+    avgWeeklyProtein,
+    avgWeeklyFat,
+    avgWeeklyCarbohydrates,
+  } = createAvgWeekNutritionValues(weeklyMealCategories)
+
+  useEffect(() => {
+    fetchWeeklyMealCategories(
+      currentDateString,
+      prevWeekDateString,
+      setWeeklyMealCategories
+    )
+  }, [currentDateString, prevWeekDateString, setWeeklyMealCategories])
+
   return (
     <Layout title="Week">
       <Box maw={300} mb={50}>
@@ -16,7 +45,12 @@ export default function Week() {
         <Text weight={200} size="xl" mb={20}>
           Weelky Nutritions
         </Text>
-        <Nutritions />
+        <Nutritions
+          dailyCalories={avgWeeklyCalories}
+          dailyProtein={avgWeeklyProtein}
+          dailyFat={avgWeeklyFat}
+          dailyCarbohydrates={avgWeeklyCarbohydrates}
+        />
       </Box>
 
       <Text weight={200} size="xl" mb={20}>
