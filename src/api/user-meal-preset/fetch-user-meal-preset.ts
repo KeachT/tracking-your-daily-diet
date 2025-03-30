2846d525d8edbdbe36ab79c0de950b0ecad4e8ad
KeachT <503.b03@gmail.com>
2025-03-31 00:47:33 +0900

    feat(api): add user meal preset functionality
    Introduced user meal preset functionality with methods to add, fetch, and update presets. This includes new API endpoints for handling user meal presets using GraphQL operations. Additionally, added export statements to ensure these methods are accessible from the API module.

---
+++ src/api/user-meal-preset/fetch-user-meal-preset.ts
@@ -0,0 +1,41 @@
+import { GraphQLQuery } from '@aws-amplify/api'
+import { API } from 'aws-amplify'
+
+import {
+  ListUserMealPresetsQuery,
+  ListUserMealPresetsQueryVariables,
+  UserMealPreset,
+} from '../../API'
+import { listUserMealPresets } from '../../graphql/queries'
+
+/**
+ * Fetches the user meal preset data.
+ * Since each user should have only one preset, we fetch the first one from the list.
+ *
+ * @param variables - Optional query variables to filter the presets.
+ * @returns The user's meal preset or null if not found.
+ */
+export const fetchUserMealPreset = async (
+  variables?: ListUserMealPresetsQueryVariables
+): Promise<UserMealPreset | null> => {
+  try {
+    const { data } = await API.graphql<GraphQLQuery<ListUserMealPresetsQuery>>({
+      query: listUserMealPresets,
+      variables,
+      authMode: 'AMAZON_COGNITO_USER_POOLS',
+    })
+
+    const userMealPresets = data?.listUserMealPresets?.items || []
+    const userMealPreset = userMealPresets[0] as UserMealPreset
+
+    if (!userMealPreset) {
+      console.warn('No user meal preset found')
+      return null
+    }
+
+    return userMealPreset
+  } catch (error) {
+    console.error('Error fetching user meal preset:', error)
+    throw error
+  }
+}
