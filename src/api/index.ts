c74bd3e474d5d6107d37b39220460911e77f8ed8
KeachT <62630204+KeachT@users.noreply.github.com>
2025-06-16 20:08:49 +0900

    feat: remove unused meal record api functions and stores (#184)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,3 @@
 export * from './daily-goal'
 export * from './daily-meal-record'
-export * from './meal-record'
 export * from './user-meal-preset'
--- src/api/meal-record/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './add-meal-record'
-export * from './fetch-meal-records'
-export * from './fetch-weekly-meal-records'
-export * from './upd-meal-record'
