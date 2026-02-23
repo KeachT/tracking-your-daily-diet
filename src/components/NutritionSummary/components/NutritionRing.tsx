c4ee95a8132a6985a5a3c2f56f86b779485658d4
KeachT <62630204+KeachT@users.noreply.github.com>
2026-02-23 18:50:28 +0900

    fix: optimize nutrition ring layout on mobile

--- src/components/NutritionSummary/components/NutritionRing.tsx
+++ src/components/NutritionSummary/components/NutritionRing.tsx
@@ -1,20 +1,22 @@
 import { Grid, RingProgress, Text } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
 
 import styles from '../style/NutritionRing.module.css'
 import { Nutrition } from '../types'
 
 export function NutritionRing({ name, number, percent, color }: Nutrition) {
   const unit = name === 'カロリー' ? 'Kcal' : 'g'
+  const isMobile = useMediaQuery('(max-width: 62em)')
 
   return (
-    <Grid.Col span={{ base: 6, md: 3 }} key={name}>
+    <Grid.Col span={{ base: 12, md: 3 }} key={name}>
       <Text fw={200} size="sm">
         {name}
       </Text>
 
       <RingProgress
-        size={140}
-        thickness={6}
+        size={isMobile ? 110 : 140}
+        thickness={isMobile ? 5 : 6}
         roundCaps
         label={
           <div className={styles.label}>
