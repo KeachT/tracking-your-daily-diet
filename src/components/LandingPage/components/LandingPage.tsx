cb89d7af09faaddf84ec7d1d9728d38d04e63599
KeachT <503.b03@gmail.com>
2026-05-02 23:45:28 +0900

    feat: add app preview section to LP and set up OGP image
    Add AppPreviewSection to the landing page that shows responsive app
    screenshots (desktop/mobile) of the daily meal tracking screen.
    Update OGP meta tags to use absolute URLs with the new OGP image,
    and add og:url, og:image dimensions, and twitter:image tags.

--- src/components/LandingPage/components/LandingPage.tsx
+++ src/components/LandingPage/components/LandingPage.tsx
@@ -5,6 +5,7 @@ import { useGuestModeStore } from '../../../stores'
 import { AuthenticatorModal } from '../../AuthenticatorModal'
 import classes from '../styles/LandingPage.module.css'
 import { AlertMessage } from './AlertMessage'
+import { AppPreviewSection } from './AppPreviewSection'
 import { FAQSection } from './FAQSection'
 import { FeatureSection } from './FeatureSection'
 import { FinalCTASection } from './FinalCTASection'
@@ -21,6 +22,7 @@ export function LandingPage() {
       <Container size="xl" className={classes.inner}>
         <Stack gap={60}>
           <HeroSection open={open} onGuestLogin={enterGuestMode} />
+          <AppPreviewSection />
           <FeatureSection />
           <PainPointSection />
           <HowToSection />
